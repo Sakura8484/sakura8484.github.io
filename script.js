c2ffa5d5b47fe15e185514fb81a85555233e3732
Sakura8484 <jocelyn_Teo@imda.gov.sg>
2024-11-13 14:48:46 +0800

    Update script.js
    javascript for form entry

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-<form action="#" method="POST">
-            <label for="name">Your Name:</label><br>
-            <input type="text" id="name" name="name" required><br><br>
-            <label for="email">Your Email:</label><br>
-            <input type="email" id="email" name="email" required><br><br>
-            <label for="message">Your Message:</label><br>
-            <textarea id="message" name="message" rows="4" required></textarea><br><br>
-            <button type="submit">Send Message</button>
-        </form>
+const statusMessage = document.getElementById('statusMessage');
+fetch('/send-email', { ... })
+  .then(response => response.json())
+  .then(data => {
+    statusMessage.textContent = 'Message sent successfully!';
+    statusMessage.style.color = 'green';
+  })
+  .catch(error => {
+    statusMessage.textContent = 'There was an error sending the message.';
+    statusMessage.style.color = 'red';
+  });
+
+
+
+
+
