57c9678be32422c457dcfbda31b74d857f646f0f
Sakura8484 <jocelyn_Teo@imda.gov.sg>
2024-11-14 15:11:29 +0800

    Update script.js

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-
 <script>
-  emailjs.init("o3b9i6py6NybD5Lr-");
+    function showForm() {
+      document.getElementById("feedbackForm").style.display = "block";
+    }
 
-  document.getElementById("myForm").addEventListener("submit", function(event) {
-    event.preventDefault();
-
-    const name = document.getElementById("name").value;   
-
-    const email = document.getElementById("email").value;
-    const message = document.getElementById("message").value;
-
-    const templateParams   
- = {
-      to_name: "Your Name",
-      from_name: name,
-      reply_to: email,
-      message: message
-    };
-
-    emailjs.send("service_15bbmrr", "template_a9cv85w", templateParams)
-      .then(function(response) {
-        console.log("SUCCESS!", response.status, response.text);   
-
-        alert("Thank you for your feedback!");
-      })
-      .catch(function(error) {
-        console.log("FAILED...", error);
-        alert("Oops, something went wrong. Please try again later.");
-      });
-  });
-</script>
+    function sendEmail() {
+      const name = document.getElementById("name").value;
+      const email = document.getElementById("email").value;
+      const   
+ message = document.getElementById("message").value;   
 
 
+      emailjs.init("o3b9i6py6NybD5Lr-");
+
+      const templateParams = {
+        to_name: "Your Name",
+        from_name: name,
+        reply_to: email,
+        message: message
+      };
+
+      emailjs.send("service_15bbmrr","template_a9cv85w", templateParams)
+        .then(function(response) {
+          console.log("SUCCESS!", response.status,   
+ response.text);
+          alert("Thank you for your feedback!");
+          document.getElementById("feedbackForm").style.display = "none";
+        })
+        .catch(function(error) {
+          console.error("FAILED...", error);
+          alert("Oops, something went wrong. Please try again later.");
+        });
+    }
+  </script>
 
 
 
