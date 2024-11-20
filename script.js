d92d70144a06b576d7a35ef43895890d980d889f
Sakura8484 <jocelyn_Teo@imda.gov.sg>
2024-11-20 11:49:10 +0800

    Update script.js
    updated code

--- script.js
+++ script.js
@@ -1,21 +1,21 @@
- <script>
-    emailjs.init("o3b9i6py6NybD5Lr-");
-
-    document.getElementById("my-form").addEventListener("submit", function(event) {
-      event.preventDefault();
-
-      emailjs.sendForm("service_15bbmrr, "Template_a9cv85w, this)
-              .then(function() {
-          console.log('SUCCESS!');   
-
-                  // Add a success message or redirect the user here
-    alert('Email sent successfully!');
-      
-    });
-  </script>
+ 
 
 
+emailjs.init("o3b9i6py6NybD5Lr-");
 
+document.getElementById("my-form").addEventListener("submit", function(event) {
+    event.preventDefault();
+    
+    emailjs.sendForm("service_15bbmrr", "template_a9cv85w", this)
+        .then(function() {
+            console.log('SUCCESS!');
+            alert('Email sent successfully!');
+        })
+        .catch(function(error) {
+            console.log('FAILED...', error);
+            alert('Failed to send email');
+        });
+});
 
 
 
